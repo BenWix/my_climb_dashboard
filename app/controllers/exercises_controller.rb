fab73cf0047454c7be31f54ed772b3aea5842774
BenWix <44454993+BenWix@users.noreply.github.com>
2020-06-20 21:18:51 -0400

    Adds ability to Edit Exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,30 @@
 class ExercisesController < ApplicationController
+    before_action :set_exercise
+    
+    def edit 
+        @holds = HOLDS
+    end
+
+    def update 
+        @exercise.update(exercise_params)
+        if @exercise.save
+            redirect_to edit_hangboard_path(@exercise.hangboard)
+        else 
+            render :edit 
+        end
+    end
+
+    def delete
+
+    end
+
+    private
+    def set_exercise 
+        @exercise = Exercise.find_by_id(params[:id])
+        
+    end
+
+    def exercise_params
+        params.require(:exercise).permit(:weight, :reps, :hold, :difficulty)
+    end
 end
