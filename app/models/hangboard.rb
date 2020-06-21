f1142a59c85f4983f016f63f945d18fcde6c5042
BenWix <44454993+BenWix@users.noreply.github.com>
2020-06-21 18:38:34 -0400

    Adds Validations and erros to hangboard

--- app/models/hangboard.rb
+++ app/models/hangboard.rb
@@ -3,6 +3,11 @@ class Hangboard < ApplicationRecord
     belongs_to :user
     accepts_nested_attributes_for :exercises
 
+    validates :weight , presence: true
+    validates_associated :user 
+    validates :date, presence: true
+    validate :hangboard_not_in_future
+
     def date=(string_date)
         date_ints = string_date.split("-").map(&:to_i)
         super(DateTime.new(*date_ints))
@@ -16,5 +21,11 @@ class Hangboard < ApplicationRecord
 
     def date_display
         self.date.strftime("%m/%d/%Y")
+    end
+
+    def hangboard_not_in_future
+        if date.present? && date > Date.today
+            errors.add(:date, "Can't be in the future")
+        end
     end
 end
